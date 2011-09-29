666bdf280b811becf84e376a9ee60f3f9dac6a60
Scott Ransom <sransom@nrao.edu>
2011-09-29 15:33:16 -0600

    Added initial files and changes to handle VCI docs.

--- multicast/make_parsers.sh
+++ multicast/make_parsers.sh
@@ -1,1 +1,2 @@
 generateDS.py --silence -o observation_mcast.py Observation.xsd
+generateDS.py --silence -o vcicommon_mcast.py vci/vciCommon.xsd
---
+++ multicast/mcast_vci.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# Send/receive UDP multicast packets.
+# Requires that your OS kernel supports IP multicast.
+#
+# Usage:
+#   mcast -s (sender, IPv4)
+#   mcast    (receivers, IPv4)
+
+port = 53000
+group = '239.192.3.1'
+MYTTL = 1 # Increase to reach other networks
+
+import time, struct, socket, sys
+import vcicommon_mcast
+
+def receiver(group, port):
+    # Look up multicast group address in name server and find out IP version
+    addrinfo = socket.getaddrinfo(group, None)[0]
+
+    # Create a socket
+    s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
+
+    # Allow multiple copies of this program on one machine
+    # (not strictly needed)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind it to the port
+    s.bind(('', port))
+
+    group_bin = socket.inet_pton(addrinfo[0], addrinfo[4][0])
+    # Join group
+    if addrinfo[0] == socket.AF_INET: # IPv4
+        mreq = group_bin + struct.pack('=I', socket.INADDR_ANY)
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    else:
+        mreq = group_bin + struct.pack('@I', 0)
+        s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+
+    # Loop, printing any data we receive
+    while True:
+        data, sender = s.recvfrom(1500)
+        while data[-1:] == '\0': data = data[:-1] # Strip trailing \0's
+        # print (str(sender) + '  ' + repr(data))
+        return data
+
+
+if __name__ == '__main__':
+    while True:
+        mcast_str = receiver(group, port)
+        print mcast_str
+        if not "AntennaPropertyTable" in mcast_str:
+            vci = vcicommon_mcast.parseString(mcast_str)
+            print "Processed a new VCI multicast..."
+            print vci.__dict__
