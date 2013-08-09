eadcbe560e09eea3f94f4c3bc13382c43fcc227e
Paul Demorest <pdemores@nrao.edu>
2013-08-08 18:07:23 -0600

    async_mcast: run commands at the specified startTime.

--- multicast/async_mcast.py
+++ multicast/async_mcast.py
@@ -3,10 +3,12 @@
 from ordereddict import OrderedDict # For python 2.6
 from psrinfo_mcast import *
 from guppi_daq import guppi_utils
-import time, struct, socket, sys, asyncore, subprocess
+from guppi_daq.astro_utils import current_MJD
+import time, struct, socket, sys, asyncore, subprocess, os
 import vcirequest_mcast
 import observation_mcast
 import netifaces
+import threading
 
 mcast_types = ["obs", "vci"]
 ports = {"obs": 53001, "vci": 53000}
@@ -14,40 +16,70 @@ groups = {"obs": '239.192.3.2', "vci": '239.192.3.1'}
 use_shmem = True
 debugout = False
 
+print "async_mcast started at ", time.ctime()
+
 # Figure out which IP addresses we might get data packets on
 data_ips = []
 for i in netifaces.interfaces():
     if 'p2p' in i:
         data_ips += [netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']]
 print "Detected IPs:", data_ips
+sys.stdout.flush()
+
+node = os.uname()[1]
 
 configs = OrderedDict()
 configstosave = 5
 
-def push_to_shmem(conf):
-    global g
-    g.read()
-    g.update("SRC_NAME", conf.source)
-    g.update("OBSERVER", conf.observer)
-    g.update("RA_STR", conf.ra_str)
-    g.update("DEC_STR", conf.dec_str)
-    g.update("TELESCOP", conf.telescope)
-    g.update("FRONTEND", conf.receiver)
-    g.update("PROJID", conf.projid)
-    g.update("FD_POLN", "CIRC")
-    g.update("TRK_MODE", "TRACK")
-    g.update("OBSFREQ", conf.skyctrfreq)
-    g.update("OBSBW", conf.bandwidth)
-    g.update("OBS_MODE", "VDIF")
-    g.update("CAL_MODE", "OFF")
-    g.update("BACKEND", conf.backend)
-    g.update("LST", conf.startLST)
+# This creates a threading.Timer instance that will run at the specified
+# time.
+def run_at(mjd, func, args=[], kwargs={}):
+    diff = (mjd - current_MJD())*86400.0
+    if diff<0.0: diff=0.0
+    print "Will execute '%s' at mjd=%f in %.1fs" % (func.__name__,
+            mjd, diff)
+    sys.stdout.flush()
+    threading.Timer(diff, func, args, kwargs).start()
+    return
+
+def guppi_daq_command(cmd):
+    guppi_ctrl = "/tmp/guppi_daq_control"
+    if os.path.exists(guppi_ctrl):
+        open(guppi_ctrl,'w').write(cmd)
+    else:
+        print "Error: guppi_daq control FIFO does not exist"
+        sys.stdout.flush()
+
+def generate_shmem_config(conf):
+    result = {}
+    result["SRC_NAME"]=conf.source
+    result["OBSERVER"]=conf.observer
+    result["RA_STR"]=conf.ra_str
+    result["DEC_STR"]=conf.dec_str
+    result["TELESCOP"]=conf.telescope
+    result["FRONTEND"]=conf.receiver
+    result["PROJID"]=conf.projid
+    result["FD_POLN"]="CIRC"
+    result["TRK_MODE"]="TRACK"
+    result["OBSFREQ"]=conf.skyctrfreq
+    result["OBSBW"]=conf.bandwidth
+    result["OBS_MODE"]="VDIF"
+    result["CAL_MODE"]="OFF"
+    result["BACKEND"]=conf.backend
+    result["LST"]=conf.startLST
     if conf.subbands[0].vdif:
         v = conf.subbands[0].vdif
-        g.update("PKTSIZE", int(v.frameSize)*4+32) # frameSize is in words
-        g.update("VDIFTIDA", int(v.aThread))
-        g.update("VDIFTIDB", int(v.bThread))
-        g.update("DATAPORT", int(v.aDestPort)) # Assume same port for both
+        result["PKTSIZE"]=int(v.frameSize)*4+32 # frameSize is in words
+        result["VDIFTIDA"]=int(v.aThread)
+        result["VDIFTIDB"]=int(v.bThread)
+        result["DATAPORT"]=int(v.aDestPort) # Assume same port for both
+    return result
+    
+def push_to_shmem(params):
+    global g
+    g.read()
+    for k in params.keys():
+        g.update(k, params[k])
     g.write()
     g.show()
     
@@ -66,33 +98,71 @@ def add_config(obj, type):
     if (hasattr(configs[obj.configId], "vci") and
         hasattr(configs[obj.configId], "obs")):
         print "Have complete config for", obj.configId
+        sys.stdout.flush()
         configs[obj.configId].parse(match_ips=data_ips)
         cc = configs[obj.configId]
         if debugout:
             print cc.__dict__
             for subband in cc.subbands:
                 print subband.__dict__
-        if use_shmem:
-            push_to_shmem(cc)
         if ('PULSAR_FOLD' in cc.scan_intent) or \
                ('PULSAR_SEARCH' in cc.scan_intent):
-            call_dspsr(cc)
-            
+            print "Pulsar config (%s)" % cc.scan_intent
+            if len(cc.subbands)>0:
+                # First generate the necessary configs:
+                shmem_conf = generate_shmem_config(cc)
+                obs_cmd = generate_obs_command(cc)
+                print "shmem conf = ", shmem_conf
+                print "obs command = '%s'" % obs_cmd
+                print "Send START at %f" % cc.startMJD
+                sys.stdout.flush()
+                # Then apply them.  This is where we should wait until
+                # the official start time:
+                run_at(cc.startMJD, run_observation, [shmem_conf, obs_cmd])
+            else:
+                print "No subbands configured"
+                sys.stdout.flush()
+        else:
+            print "Non-pulsar config (%s)" % cc.scan_intent
+            print "Send STOP at %f" % cc.startMJD
+            sys.stdout.flush()
+            # If this is a non-pulsar config we need to stop 
+            # any running pulsar processing.
+            # This should also not be done until the appropriate time
+            run_at(cc.startMJD, stop_observation)
 
-def call_dspsr(conf):
+def generate_obs_command(conf):
+    output_file = "/lustre/evla/pulsar/data/%s.%s.%s.%s" % (conf.source,
+            conf.projid, conf.seq, node)
     if 'PULSAR_FOLD' in conf.scan_intent:
-        # construct command line, referring to .par file
-        # and building the output file name from what should
-        # be unique identifiers [TBC]
-        command_args = ('dspsr -header INSTRUMENT=guppi_daq DATABUF=1 -a PSRFITS -minram=1 -t6 -F256:D -L10. -E /lustre/evla/pulsar/data/%s.par -b 1024 -O /lustre/evla/pulsar/data/%s.%s.%s' % (conf.source, conf.source, conf.projid, conf.seq)).split(' ')
-    else:  # PULSAR_SEARCH 
-        # construct command line, building the output file name
-        # from what should be unique identifiers [TBC]
-        command_line = 'digifil -F1024:D -o /lustre/evla/pulsar/%s.%s.FITS' % (conf.projid, conf.seq)
-    print "Pulsar observation!  Command: ", command_args
-    #subprocess.Popen(command_args)
-    #time.sleep(1)
-    #open('/tmp/guppi_daq_control','w').write('START')
+        # Fold command line
+        command = 'dspsr -a PSRFITS -minram=1 -t8 -F32:D -L10. -E/lustre/evla/pulsar/data/%s.par -b1024 -O%s' % (conf.source, output_file)
+    elif 'PULSAR_SEARCH' in conf.scan_intent:
+        # Search command line
+        command = 'digifil -B16 -F256 -t128 -b8 -c -o%s.fil' % (output_file)
+    else:
+        print "Unrecognized pulsar intent='%s'" % conf.scan_intent
+        return
+    header_args = " -header INSTRUMENT=guppi_daq DATABUF=1"
+    command += header_args
+    return command
+
+def run_observation(shmem, command):
+    stop_observation() # First make sure we're stopped
+    push_to_shmem(shmem)
+    print "Pulsar observation!  Command: ", command
+    sys.stdout.flush()
+    subprocess.Popen(command.split(' '))
+    time.sleep(1)
+    guppi_daq_command('START')
+
+def stop_observation():
+    print "Stop observations"
+    sys.stdout.flush()
+    guppi_daq_command('STOP')
+    # These should probably be done in a cleaner way:
+    os.system("pkill -TERM dspsr")
+    os.system("pkill -TERM digifil")
 
 class mcast_client(asyncore.dispatcher):
 
