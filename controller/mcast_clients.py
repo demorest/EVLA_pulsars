b592eb4ef067163ec330e4a40055a370ccbaf149
Paul Demorest <pdemores@nrao.edu>
2015-07-09 17:11:22 -0600

    mcast_clients: add url read option for vci retrieval

--- controller/mcast_clients.py
+++ controller/mcast_clients.py
@@ -11,6 +11,7 @@ import os
 import struct
 import logging
 import asyncore, socket
+import urllib
 import vcixml_parser
 import obsxml_parser
 
@@ -59,16 +60,32 @@ class ObsClient(McastClient):
 
     If the controller input is given, the controller.add_obs(obs) method will
     be called for every document received.
+
+    If use_configUrl is true, the VCI will be retrieved from the url given
+    in the Observation document, parsed, and controller.add_vci(vci) will
+    be called if a controller exists.
     """
 
-    def __init__(self,controller=None):
+    def __init__(self,controller=None,use_configUrl=False):
         McastClient.__init__(self,'239.192.3.2',53001,'obs')
         self.controller = controller
+        self.use_configUrl = use_configUrl
 
     def parse(self):
         obs = obsxml_parser.parseString(self.read)
         logging.info("read obs configId='%s' seq=%d" % (obs.configId,
             obs.seq))
+        if self.use_configUrl:
+            logging.info("retrieve vci from '%s'" % obs.configUrl)
+            try:
+                vciread = urllib.urlopen(obs.configUrl).read()
+                logging.debug('retrieved vci ' + vciread)
+                vci = vcixml_parser.parseString(vciread)
+                if self.controller is not None:
+                    self.controller.add_vci(vci)
+            except Exception as e:
+                logging.exception("error retrieving VCI from '%s'" 
+                        % obs.configUrl)
         if self.controller is not None:
             self.controller.add_obs(obs)
 
@@ -99,7 +116,7 @@ if __name__ == '__main__':
     logging.basicConfig(format="%(asctime)-15s %(levelname)8s %(message)s",
             level=logging.DEBUG)
     vci_client = VCIClient()
-    obs_client = ObsClient()
+    obs_client = ObsClient(use_configUrl=True)
     try:
         asyncore.loop()
     except KeyboardInterrupt:
